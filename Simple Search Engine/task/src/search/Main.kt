6ff229fc72e7fa232aad70918cca4f45852cbaa9
Treboder <robert.kohn@gmx.net>
2022-07-28 23:03:10 +0200

    Stage 2/6: Expand the search

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -2,16 +2,58 @@ package search
 
 fun main() {
 
+    println("Enter the number of people:")
     print("> ")
-    var input = readLine()!!.split(' ')
+    var inputLines = readLine()!!.toInt()
+
+    var inputs = mutableListOf<List<String>>(listOf<String>())
+    println("Enter all people:")
+    repeat(inputLines)
+    {
+        print("> ")
+        inputs.add(readLine()!!.split(' '))
+    }
+
+    println()
+    println("Enter the number of search queries:")
     print("> ")
-    var item = readLine()!!
+    var queries = readLine()!!.toInt()
+
+    repeat(queries)
+    {
+        println()
+        println("Enter data to search people:")
+        print("> ")
+        var term = readLine()!!
+        var results = search(term, inputs)
+        if(results.size>0)
+        {
+            println()
+            println("People found:")
+            for(line in results)
+                println(line)
+
+        }
+        else
+            println("No matching people found.")
+
+    }
 
 
-    var position = input.indexOf(item)+1 
-    if(position != 0)
-        println(position)
-    else            
-        println("Not found")
 
+}
+
+fun search(term:String, inputs:List<List<String>>):List<String> {
+
+    var results = mutableListOf<String>()
+    for(line in inputs)
+        for(item in line)
+        {
+            if(item.lowercase() == term.lowercase() || item.contains(term, ignoreCase = true))
+            {
+                results.add(line.joinToString(" "))
+                break
+            }
+        }
+    return results
 }
