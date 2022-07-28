7442e4dd87b35f51d1c4e5a6dde8cdc6c8346fe4
Treboder <robert.kohn@gmx.net>
2022-07-28 23:17:27 +0200

    Stage 2/6: Expand the search (refactored)

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -2,58 +2,50 @@ package search
 
 fun main() {
 
+    // clarify how many lines of input to be used
     println("Enter the number of people:")
     print("> ")
-    var inputLines = readLine()!!.toInt()
+    var numberOfInputLines = readLine()!!.toInt()
 
-    var inputs = mutableListOf<List<String>>(listOf<String>())
+    // fill input matrix line by line
+    var inputMatrix = mutableListOf<List<String>>(listOf<String>())
     println("Enter all people:")
-    repeat(inputLines)
-    {
+    repeat(numberOfInputLines) {
         print("> ")
-        inputs.add(readLine()!!.split(' '))
+        inputMatrix.add(readLine()!!.split(' '))
     }
 
+    // clarify how many search queries to perform
     println()
     println("Enter the number of search queries:")
     print("> ")
-    var queries = readLine()!!.toInt()
+    var numberOfSearchQueries = readLine()!!.toInt()
 
-    repeat(queries)
-    {
+    // perform search queries in sequence
+    repeat(numberOfSearchQueries) {
         println()
         println("Enter data to search people:")
         print("> ")
-        var term = readLine()!!
-        var results = search(term, inputs)
-        if(results.size>0)
-        {
+        var termToSearch = readLine()!!
+        var searchResults = search(termToSearch, inputMatrix)
+        if(searchResults.size > 0) {
             println()
             println("People found:")
-            for(line in results)
+            for(line in searchResults)
                 println(line)
-
         }
         else
             println("No matching people found.")
-
     }
-
-
-
 }
 
 fun search(term:String, inputs:List<List<String>>):List<String> {
-
     var results = mutableListOf<String>()
     for(line in inputs)
         for(item in line)
-        {
-            if(item.lowercase() == term.lowercase() || item.contains(term, ignoreCase = true))
-            {
+            if(item.contains(term, ignoreCase = true)) {
                 results.add(line.joinToString(" "))
                 break
             }
-        }
     return results
 }
