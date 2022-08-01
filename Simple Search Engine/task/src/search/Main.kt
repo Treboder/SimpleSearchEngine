b373004b71a43c6681e1c59db870781d28de42da
Treboder <robert.kohn@gmx.net>
2022-08-01 09:26:38 +0200

    Stage 6/6: Search strategies

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -8,26 +8,20 @@ fun main(args: Array<String>) {
 
     // specify run mode, either in debugging mode or as desired with main args
     val debugging = false
-
     // desired usage with main args from command prompt, skip this in debugging mode
     if(!debugging)
         Interpreter.processArguments(args.toMutableList())
-
     // simulate program input via args array in debugging mode, otherwise skip
-    while(debugging) {
+    if(debugging) {
         print("args: ")
         val arguments = mutableListOf<String>()
         arguments.addAll(readLine()!!.split(" "))
         Interpreter.processArguments(arguments)
-        if(arguments[0] == "done")
-            break
     }
-
     // main loop with user menu
     var repeat = true
     while(repeat)
         repeat = Interpreter.userMenuLoop()
-
     println()
     println("Bye!")
 }
@@ -65,7 +59,7 @@ object Interpreter {
 }
 
 object InputData {
-    var contentList2D = mutableListOf(listOf<String>()) // list if lines where each line itself is a list of words
+    var listOfLinesWithListOfWords = mutableListOf(listOf<String>()) // list if lines where each line itself is a list of words
     var mapPairingWordsWithListOfIndices = mutableMapOf<String, MutableList<Int>>() // map that pairs every word from input with a list of line indices (corresponding with contentList2D)
 
     fun initializeWithFile(fileName: String) {
@@ -78,7 +72,7 @@ object InputData {
         try {
             val lines = File(rootDirectory.absolutePath + "\\" +name).readLines()
             for(line in lines)
-                contentList2D.add(line.split(' '))
+                listOfLinesWithListOfWords.add(line.split(' '))
         }
         catch (e: IOException)  {
             println("Can't read input file!")
@@ -86,8 +80,8 @@ object InputData {
     }
 
     private fun createContentMap() {
-        for(lineIndex in 0 until contentList2D.count())
-            for (word in contentList2D[lineIndex])
+        for(lineIndex in 0 until listOfLinesWithListOfWords.count())
+            for (word in listOfLinesWithListOfWords[lineIndex])
                 if(!mapPairingWordsWithListOfIndices.containsKey(word.lowercase()))
                     mapPairingWordsWithListOfIndices.put(word.lowercase(), mutableListOf(lineIndex))
                 else
@@ -97,25 +91,85 @@ object InputData {
     fun printAllPeople() {
         println("")
         println("=== List of people ===")
-        for(line in contentList2D)
+        for(line in listOfLinesWithListOfWords)
             println(line.joinToString(" "))
     }
 }
 
 object SearchEngine {
     fun findPerson() {
+        val (searchMode, termToSearch) = getSearchQueryAndSearchMode()
+        val foundIndices = mutableListOf<Int>()
+        when(searchMode) {
+            "ALL" -> foundIndices.addAll(findAll(termToSearch))
+            "ANY" -> foundIndices.addAll(findAny(termToSearch))
+            "NONE" -> foundIndices.addAll(findNone(termToSearch))
+            else -> println("$searchMode is unknown command.")
+        }
+        printSearchResults(foundIndices)
+    }
+
+    private fun getSearchQueryAndSearchMode(): Pair<String, String> {
+        println()
+        println("Select a matching strategy: ALL, ANY, NONE")
+        val searchMode = readLine()!!
         println()
         println("Enter a name or email to search all matching people.")
-        //print("> ")
         val termToSearch = readLine()!!.lowercase()
-        //val searchResults = searchForInputLinesThatContainTheTerm(termToSearch)
-        if(InputData.mapPairingWordsWithListOfIndices.containsKey(termToSearch)) {
-            val foundIndices = InputData.mapPairingWordsWithListOfIndices[termToSearch]
-            println("${foundIndices!!.count()} persons found:")
-            for (index in foundIndices)
-                println(InputData.contentList2D[index].joinToString(" "))
+        return Pair(searchMode, termToSearch)
+    }
+
+    private fun findAll(termToSearch:String):MutableList<Int> {
+        val wordsToSearch = termToSearch.split(" ")
+        val lineIndicesToReturn = mutableListOf<Int>()
+        for(lineIndex in 0 until InputData.listOfLinesWithListOfWords.size) {
+            var lineContainsAllWords = true
+            for(word in wordsToSearch) {
+                if(!InputData.listOfLinesWithListOfWords[lineIndex].joinToString().contains(word, ignoreCase = true))
+                    lineContainsAllWords = false
+            }
+            if(lineContainsAllWords)
+                lineIndicesToReturn.add(lineIndex)
         }
-        else
+        return lineIndicesToReturn
+    }
+
+    private fun findAny(termToSearch:String):MutableList<Int> {
+        val wordsToSearch = termToSearch.split(" ")
+        val lineIndicesToReturn = mutableListOf<Int>()
+        for(word in wordsToSearch)
+            lineIndicesToReturn.addAll(findWord(word))
+        return lineIndicesToReturn
+    }
+
+    private fun findNone(termToSearch:String):MutableList<Int> {
+        val wordsToSearch = termToSearch.split(" ")
+        val lineIndicesToReturn = mutableListOf<Int>()
+        for(lineIndex in 0 until InputData.listOfLinesWithListOfWords.size) {
+            var lineContainsNoWord = true
+            for(word in wordsToSearch) {
+                if(InputData.listOfLinesWithListOfWords[lineIndex].joinToString().contains(word, ignoreCase = true))
+                    lineContainsNoWord = false
+            }
+            if(lineContainsNoWord)
+                lineIndicesToReturn.add(lineIndex)
+        }
+        return lineIndicesToReturn
+    }
+
+    private fun findWord(termToSearch:String): MutableList<Int> {
+        val foundIndices = mutableListOf<Int>()
+        if(InputData.mapPairingWordsWithListOfIndices.containsKey(termToSearch))
+            foundIndices.addAll(InputData.mapPairingWordsWithListOfIndices[termToSearch]!!)
+        return foundIndices
+    }
+
+    private fun printSearchResults(foundIndices: MutableList<Int>) {
+        if (foundIndices.isNotEmpty()) {
+            println("${foundIndices.count()} persons found:")
+            for (index in foundIndices)
+                println(InputData.listOfLinesWithListOfWords[index].joinToString(" "))
+        } else
             println("No matching people found.")
     }
 }
